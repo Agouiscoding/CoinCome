bf657953f0a0d09c86ac96e0ef5df9c80436824a
yangyzs <171981480+yangyzs@users.noreply.github.com>
2025-11-23 16:37:12 -0500

    built dashboard basics

--- coincome-frontend/src/router/index.js
+++ coincome-frontend/src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard', // 修改为 path
+      name: 'dashboard',
+      component: () => import('@/components/dashboard/DashboardView.vue')
+    }
+  ],
 })
 
 export default router
